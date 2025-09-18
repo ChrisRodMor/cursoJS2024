d4ec27c27f5d8a77777c87fbd6e2ea6c87971acc
Christian Rodriguez <christian2_21@alu.uabcs.mx>
2025-09-17 17:08:09 -0700

    Comentar codigo

--- Curso JS/01-Introduccion/js/script.js
+++ Curso JS/01-Introduccion/js/script.js
@@ -1,3 +1,11 @@
+// Pide el nombre del usuario
 const nombre = prompt('¿Cuál es tu nombre?');
-// este query selector te permite seleccionar elementos del HTML
-document.querySelector('.contenido').innerHTML = `${nombre}, estás aprendiendo JavaScript Moderno</h2>`;
+
+// este querySelector te permite seleccionar elementos del HTML
+// innerHTML permite insertar código HTML
+// las comillas invertidas permiten interpolar variables con ${}
+// Esta funcion, al ejecutarse, pide el nombre del usuario y lo inserta en el HTML
+document.querySelector('.contenido').innerHTML = `${nombre}, estás aprendiendo JavaScript Moderno`;
+
+// Asigna el valor hacia la variable producto
+const producto = 'Monitor 24 pulgadas';
