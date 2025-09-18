5c2107ecc5a9ea7a473209ac10b7232e293b0552
Christian Rodriguez <christian2_21@alu.uabcs.mx>
2025-09-17 18:49:36 -0700

    let

---
+++ Curso JS/02-Variables/js/02-app.js
@@ -0,0 +1,6 @@
+let producto = "Tablet"; // Iniciar variable y asignarle un valor
+let disponible; // Iniciar variable sin valor (undefined)
+producto = "Monitor"; // Reasignando el valor de la variable
+producto = 20; // Reasignando el valor de la variable
+producto = true; // Reasignando el valor de la variable
+producto = null; // Reasignando el valor de la variable
