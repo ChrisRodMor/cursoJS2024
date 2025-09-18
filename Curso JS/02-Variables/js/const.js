aafdc709d8b2dc1ceb1a357ec46a3d24c002892c
Christian Rodriguez <christian2_21@alu.uabcs.mx>
2025-09-17 19:03:03 -0700

    const

---
+++ Curso JS/02-Variables/js/const.js
@@ -0,0 +1,9 @@
+//const no puede ser reasignada y debe ser inicializada con un valor
+const nombre = "Chrixnux";
+//nombre = "Juan"; // Esto causará un error
+console.log(nombre);
+
+//en cambio con let si se puede reasignar
+let apellido = "Perez";
+apellido = "Gomez";
+console.log(apellido);
