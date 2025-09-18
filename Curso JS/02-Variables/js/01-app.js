9a58ee9840f08a7de4a706946d50e085b2af6b7d
Christian Rodriguez <christian2_21@alu.uabcs.mx>
2025-09-17 18:44:05 -0700

    var

---
+++ Curso JS/02-Variables/js/01-app.js
@@ -0,0 +1,13 @@
+var disponible; // Inicializar una variable sin valor (undefined)
+var producto = "Monitor de 24 Pulgadas"; //Inicializar una variable y asignarle un valor
+producto = "Monitor de 19 Pulgadas"; // Reasignando el valor
+producto = 20; // Ahora es un número
+producto = true; // Ahora es un booleano verdadero
+producto = false; // Ahora es un booleano falso
+producto = null; // Ahora es null (ausencia de valor)
+
+// inicializar múltiples variables
+var categoria = "Computadoras",
+    marca = "Marca Famosa",
+    calificacion = 5;
+
