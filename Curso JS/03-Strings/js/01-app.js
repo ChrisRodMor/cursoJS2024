549fdc0352dea9df73cafdfa2ee4f9099c394702
Christian Rodriguez <christian2_21@alu.uabcs.mx>
2025-09-17 19:15:20 -0700

    Strings

---
+++ Curso JS/03-Strings/js/01-app.js
@@ -0,0 +1,19 @@
+//String v1
+const nombre = "Juan";
+console.log(nombre);
+
+//String v2
+const nombre2 = String('Carlos');
+console.log(nombre2);
+
+//crear un objeto de tipo String
+const nombre3 = new String("Juan");
+console.log(nombre3);
+
+//agregar comillas en string v1
+let producto = "Monitor 20\"";
+//agregar comillas en string v2
+producto = 'Monitor 20"';
+//agregar comillas en string v3
+producto = `Monitor 20"`;
+console.log(producto);
